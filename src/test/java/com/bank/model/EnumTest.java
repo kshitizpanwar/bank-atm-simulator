b0923ea0b032c93a9be7940a66a4fff7a66a283f
Kshitiz Panwar <kshitizpanwar19@gmail.com>
2026-07-10 21:29:36 +0530

    test(model): cover all enum constants in round-trip test

--- src/test/java/com/bank/model/EnumTest.java
+++ src/test/java/com/bank/model/EnumTest.java
@@ -5,9 +5,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EnumTest {
     @Test
-    void enumsRoundTripThroughName() {
+    void accountTypeRoundTripAndLength() {
+        assertEquals(2, AccountType.values().length);
         assertEquals(AccountType.SAVINGS, AccountType.valueOf("SAVINGS"));
+        assertEquals(AccountType.CURRENT, AccountType.valueOf("CURRENT"));
+    }
+
+    @Test
+    void accountStatusRoundTripAndLength() {
+        assertEquals(3, AccountStatus.values().length);
         assertEquals(AccountStatus.ACTIVE, AccountStatus.valueOf("ACTIVE"));
+        assertEquals(AccountStatus.BLOCKED, AccountStatus.valueOf("BLOCKED"));
+        assertEquals(AccountStatus.CLOSED, AccountStatus.valueOf("CLOSED"));
+    }
+
+    @Test
+    void transactionTypeRoundTripAndLength() {
+        assertEquals(3, TransactionType.values().length);
         assertEquals(TransactionType.DEPOSIT, TransactionType.valueOf("DEPOSIT"));
+        assertEquals(TransactionType.WITHDRAW, TransactionType.valueOf("WITHDRAW"));
+        assertEquals(TransactionType.TRANSFER, TransactionType.valueOf("TRANSFER"));
     }
 }
